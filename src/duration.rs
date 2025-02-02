acffb90c7fe7b097f979de7f15b8c00cd24dd555
Ashwin Naren <arihant2math@gmail.com>
2025-02-01 18:52:52 -0800

    fix enum numbering

--- src/duration.rs
+++ src/duration.rs
@@ -1,42 +1,13 @@
 #[derive(Copy, Clone, Debug)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 pub enum PrimitiveDuration {
-    Whole,
-    Half,
-    Quarter,
-    Eighth,
-    Sixteenth,
-    ThirtySecond,
-    SixtyFourth,
-}
-
-impl From<u8> for PrimitiveDuration {
-    fn from(value: u8) -> Self {
-        match value {
-            1 => PrimitiveDuration::Whole,
-            2 => PrimitiveDuration::Half,
-            4 => PrimitiveDuration::Quarter,
-            8 => PrimitiveDuration::Eighth,
-            16 => PrimitiveDuration::Sixteenth,
-            32 => PrimitiveDuration::ThirtySecond,
-            64 => PrimitiveDuration::SixtyFourth,
-            _ => panic!("Invalid duration value: {}", value),
-        }
-    }
-}
-
-impl Into<u8> for PrimitiveDuration {
-    fn into(self) -> u8 {
-        match self {
-            PrimitiveDuration::Whole => 1,
-            PrimitiveDuration::Half => 2,
-            PrimitiveDuration::Quarter => 4,
-            PrimitiveDuration::Eighth => 8,
-            PrimitiveDuration::Sixteenth => 16,
-            PrimitiveDuration::ThirtySecond => 32,
-            PrimitiveDuration::SixtyFourth => 64,
-        }
-    }
+    Whole = 1,
+    Half = 2,
+    Quarter = 4,
+    Eighth = 8,
+    Sixteenth = 16,
+    ThirtySecond = 32,
+    SixtyFourth = 64,
 }
 
 #[derive(Copy, Clone, Debug)]
