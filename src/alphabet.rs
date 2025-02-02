5b321fbc7779aa5b7829b292dec9996939ab9a36
Ashwin Naren <arihant2math@gmail.com>
2025-02-01 20:38:13 -0800

    more docs for alphabet

--- src/alphabet.rs
+++ src/alphabet.rs
@@ -1,5 +1,9 @@
 use std::fmt::{Debug, Display};
 
+/// Represents an alphabet of the musical alphabet (A-G).
+/// This should not be used to represent a pitch.
+/// For relative pitches [Solfege](crate::solfege::Solfege) should be used.
+/// For absolute pitches [NoteValue](crate::note::NoteValue) should be used.
 #[derive(Copy, Clone)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 pub enum Alphabet {
