06bf426d80824ac15d7bcc9e5676a3eec0b0e05e
Ashwin Naren <arihant2math@gmail.com>
2025-02-01 17:35:55 -0800

    added conversions to and from u8 for PrimitiveDuration

--- src/duration.rs
+++ src/duration.rs
@@ -9,6 +9,35 @@ pub enum PrimitiveDuration {
     SixtyFourth,
 }
 
+impl From<u8> for PrimitiveDuration {
+    fn from(value: u8) -> Self {
+        match value {
+            1 => PrimitiveDuration::Whole,
+            2 => PrimitiveDuration::Half,
+            4 => PrimitiveDuration::Quarter,
+            8 => PrimitiveDuration::Eighth,
+            16 => PrimitiveDuration::Sixteenth,
+            32 => PrimitiveDuration::ThirtySecond,
+            64 => PrimitiveDuration::SixtyFourth,
+            _ => panic!("Invalid duration value: {}", value),
+        }
+    }
+}
+
+impl Into<u8> for PrimitiveDuration {
+    fn into(self) -> u8 {
+        match self {
+            PrimitiveDuration::Whole => 1,
+            PrimitiveDuration::Half => 2,
+            PrimitiveDuration::Quarter => 4,
+            PrimitiveDuration::Eighth => 8,
+            PrimitiveDuration::Sixteenth => 16,
+            PrimitiveDuration::ThirtySecond => 32,
+            PrimitiveDuration::SixtyFourth => 64,
+        }
+    }
+}
+
 #[derive(Copy, Clone, Debug)]
 pub struct Duration {
     pub primitive: PrimitiveDuration,
